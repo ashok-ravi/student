a4b5083c1e441f8ba47bff7f56f7aa749466169a
Ashok Ravi <ashokkumar.raavi@gmail.com>
2017-10-29 20:04:35 +0530

    Creating Java Project

---
+++ stdent-services/src/main/java/com/in28minues/springboot/stdentservices/StdentServicesApplication.java
@@ -0,0 +1,12 @@
+package com.in28minues.springboot.stdentservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StdentServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StdentServicesApplication.class, args);
+	}
+}
---
+++ stdent-services/src/test/java/com/in28minues/springboot/stdentservices/StdentServicesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.in28minues.springboot.stdentservices;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StdentServicesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
